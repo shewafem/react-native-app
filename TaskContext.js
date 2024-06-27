85f4b71a20b0b78f36cd6bc80e828d7a97a28ca7
shewafem <tamirsundureev@gmail.com>
2024-06-28 06:53:39 +0800

    added taskcontext

--- TaskContext.js
+++ TaskContext.js
@@ -3,20 +3,29 @@ import React, { createContext, useState } from 'react';
 export const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
+    //Определяем задачи, используем useState hook
     const [tasks, setTasks] = useState([
         { id: 1, text: "Сделать домашку", completed: true },
         { id: 2, text: "Сходить в зал", completed: false },
     ]);
 
+    /*Функция для добавления задачи.
+    Создается newTask с введённым текстом, 
+    id = текущая дата, не выполнена изначально.
+    Далее setTasks добавляет задачу в tasks*/
     const addTask = (text) => {
         const newTask = { id: Date.now(), text, completed: false };
         setTasks([...tasks, newTask]);
     };
 
+    /*Функция для удаления задачи.
+    Передается id задачи и фильтр массива tasks*/
     const deleteTask = (id) => {
         setTasks(tasks.filter((task) => task.id !== id));
     };
 
+    /*Функция переключения состояния задачи.
+    Передается id задачи и происходит изменение состояния task*/
     const toggleCompleted = (id) => {
         setTasks(
             tasks.map((task) =>
@@ -25,8 +34,20 @@ export const TaskProvider = ({ children }) => {
         );
     };
 
+    /*Функция редактирования задачи.
+    Передается id задачи и новый текст, происходит изменение текста task*/
+    const editTask = (id, newText) => {
+        setTasks(
+            tasks.map((task) =>
+                task.id === id ? { ...task, text: newText } : task
+            )
+        );
+    };
+
     return (
-        <TaskContext.Provider value={{ tasks, addTask, deleteTask, toggleCompleted }}>
+        /*Определяем TaskProvider который будет давать 
+        доступ к данным контекста своим детям*/
+        <TaskContext.Provider value={{ tasks, addTask, deleteTask, toggleCompleted, editTask}}>
             {children}
         </TaskContext.Provider>
     );
